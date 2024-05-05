c02dc56ed4a3387cbde13fd348b71c143ca422f5
Mauricio Arcanjo <mau.arcanjo@gmail.com>
2024-05-05 13:54:16 -0400

    Implementada a função do botão salvar da tela de cadastro

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,45 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-
+import java.sql.SQLException;
 
 public class ProdutosDAO {
-    
+
+    //conectaDAO conexao;
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) {
+
+        conn = new conectaDAO().connectDB();
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?);");
+
+            stmt.setString(1, produto.getNome());
+            stmt.setInt(2, produto.getValor());
+            stmt.setString(3, produto.getStatus());
+            stmt.executeUpdate();
+
+            JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso!");
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Ocorreu um erro ao tentar cadastrar!");
+            System.out.println(ex);
+        }
+
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
