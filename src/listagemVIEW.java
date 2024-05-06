4d47f713d7a0883c328d9310d5bc2f47d0941191
Mauricio Arcanjo <mau.arcanjo@gmail.com>
2024-05-06 18:17:48 -0400

    Implementada funcionalidade ao botão de Consultar Vendas na tela de listagem

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -145,6 +145,7 @@ public class listagemVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
+        //Implementação da função de consultar vendas
         vendasVIEW vendas = new vendasVIEW(); 
         vendas.setVisible(true);
     }//GEN-LAST:event_btnVendasActionPerformed
