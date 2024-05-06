f64e5b40f39e777f4e7c043bbdbb4b5cfa73ae36
Mauricio Arcanjo <mau.arcanjo@gmail.com>
2024-05-06 18:14:18 -0400

    Implementada funcionalidade ao botão de Vender Produto na tela de listagem

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -137,7 +137,7 @@ public class listagemVIEW extends javax.swing.JFrame {
 
     private void btnVenderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVenderActionPerformed
         String id = id_produto_venda.getText();
-        
+        //Implementação da função de vender produto
         ProdutosDAO produtosdao = new ProdutosDAO();
         
         produtosdao.venderProduto(Integer.parseInt(id));
